902ae1f78ae950c8701d6f06b6a2eb11dcb22d04
jeselirod <jesuselia71@gmail.com>
2019-04-01 16:54:52 +0200

    Objeto formulario de hacker mas su servicio

--- Acme-Hacker-Rank/src/main/java/forms/RegistrationFormHacker.java
+++ Acme-Hacker-Rank/src/main/java/forms/RegistrationFormHacker.java
@@ -10,11 +10,16 @@
 
 package forms;
 
+import java.util.HashSet;
+
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import security.Authority;
+import security.UserAccount;
 import domain.Actor;
+import domain.CredictCard;
 import domain.Finder;
 
 public class RegistrationFormHacker extends Actor {
@@ -24,7 +29,7 @@ public class RegistrationFormHacker extends Actor {
 	}
 
 
-	private String	password2;
+	private String	password;
 	private Boolean	check;
 	private Boolean	patternPhone;
 	private Finder	finder;
@@ -49,12 +54,12 @@ public class RegistrationFormHacker extends Actor {
 	}
 
 	@Size(min = 5, max = 32)
-	public String getPassword2() {
-		return this.password2;
+	public String getPassword() {
+		return this.password;
 	}
 
-	public void setPassword2(final String password2) {
-		this.password2 = password2;
+	public void setPassword(final String password) {
+		this.password = password;
 	}
 
 	public Boolean getCheck() {
@@ -63,6 +68,38 @@ public class RegistrationFormHacker extends Actor {
 
 	public void setCheck(final Boolean check) {
 		this.check = check;
+	}
+
+	public RegistrationFormHacker createToHacker() {
+
+		final RegistrationFormHacker registrationForm = new RegistrationFormHacker();
+		registrationForm.setFinder(new Finder());
+		registrationForm.setCheck(false);
+		registrationForm.setPatternPhone(false);
+		registrationForm.setAddress("");
+		registrationForm.setEmail("");
+		registrationForm.setName("");
+		registrationForm.setVatNumber("");
+		registrationForm.setPhoto("");
+		registrationForm.setSurnames(new HashSet<String>());
+		registrationForm.setAddress("");
+		registrationForm.setCredictCard(new CredictCard());
+		registrationForm.setPassword("");
+
+		//PREGUNTAR
+		final UserAccount user = new UserAccount();
+		user.setAuthorities(new HashSet<Authority>());
+		final Authority ad = new Authority();
+		ad.setAuthority(Authority.HACKER);
+		user.getAuthorities().add(ad);
+
+		//NUEVO
+		user.setUsername("");
+		user.setPassword("");
+
+		registrationForm.setUserAccount(user);
+
+		return registrationForm;
 	}
 
 }
