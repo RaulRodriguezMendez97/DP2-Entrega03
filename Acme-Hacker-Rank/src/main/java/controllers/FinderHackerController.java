e92bb14c176ba08d92a1e997621f23da884ca729
AntSegJim <antonio.alandalus97@gmail.com>
2019-04-01 19:19:26 +0200

    a

---
+++ Acme-Hacker-Rank/src/main/java/controllers/FinderHackerController.java
@@ -0,0 +1,52 @@
+
+package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.FinderService;
+import domain.Finder;
+
+@Controller
+@RequestMapping("/finder/hacker")
+public class FinderHackerController extends AbstractController {
+
+	@Autowired
+	private FinderService	finderService;
+
+
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit() {
+		final ModelAndView result;
+		final Finder finder = this.finderService.findOne();
+
+		result = new ModelAndView("finder/edit");
+		result.addObject("finder", finder);
+		return result;
+
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
+	public ModelAndView edit(final Finder f, final BindingResult binding) {
+		final ModelAndView result;
+		final Finder finder;
+
+		finder = this.finderService.reconstruct(f, binding);
+
+		if (!binding.hasErrors()) {
+			this.finderService.save(finder);
+			result = new ModelAndView("edit.do");
+		} else {
+			result = new ModelAndView("finder/edit");
+			result.addObject("finder", finder);
+		}
+
+		return result;
+
+	}
+
+}
