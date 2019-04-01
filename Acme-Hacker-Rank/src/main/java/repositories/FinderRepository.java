0ac1e8e94952e29648e7a923e1665da6b023a17a
AntSegJim <antonio.alandalus97@gmail.com>
2019-04-01 18:09:47 +0200

    fixed FinderRespository

--- Acme-Hacker-Rank/src/main/java/repositories/FinderRepository.java
+++ Acme-Hacker-Rank/src/main/java/repositories/FinderRepository.java
@@ -14,7 +14,7 @@ import domain.Position;
 @Repository
 public interface FinderRepository extends JpaRepository<Finder, Integer> {
 
-	@Query("select h.finder from Hacer h where h.userAccount.id = ?1")
+	@Query("select h.finder from Hacker h where h.userAccount.id = ?1")
 	public Finder getMyFinder(int id);
 
 	@Query("select p from Position p where (locate(?1,p.title) != 0 or locate(?1,p.ticker) != 0 or locate(?1,p.description) != 0 or locate(?1,p.skillsRequired) != 0 or locate(?1,technologiesRequired) or locate(?1,requiredProfile)) and p.deadline between ?4 and ?5 and p.salary between ?2 and ?3")
