20274525bd18c9c8d8c5093380b433bb35ee220d
jeselirod <jesuselia71@gmail.com>
2019-04-20 18:42:13 +0200

    Exportacion de datos implementada

--- Acme-Hacker-Rank/src/main/java/repositories/HackerRepository.java
+++ Acme-Hacker-Rank/src/main/java/repositories/HackerRepository.java
@@ -18,4 +18,5 @@ public interface HackerRepository extends JpaRepository<Hacker, Integer> {
 	//DASHBOARD
 	@Query("select h.name from Hacker h where (select max(1.0*( select count(a.hacker) from Application a where a.hacker.id=x.id)) from Hacker x)=(1.0*(select count(b.hacker) from Application b where b.hacker.id=h.id))")
 	public List<String> getHackersWithMoreApplications();
+
 }
